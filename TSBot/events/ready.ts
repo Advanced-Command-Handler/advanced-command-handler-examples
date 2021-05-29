8436520d5f500199c2fafc3c13e30d22555a6762
Ayfri <pierre.ayfri@gmail.com>
2021-05-29 04:41:40 +0200

    feat: Update ready event to ACH 2.4.0.

--- TSBot/events/ready.ts
+++ TSBot/events/ready.ts
@@ -1,19 +1,32 @@
-import {CommandHandler, Logger} from 'advanced-command-handler';
+import {CommandHandler, Event, Logger} from 'advanced-command-handler';
+export default new Event(
+	{
+		name: 'ready',
+		once: true,
+	},
+	async (handler: typeof CommandHandler) => {
+		function log() {
+			Logger.event(
+				`Date : ${Logger.setColor('yellow', new Date().toString())}`
+			);
+			Logger.event(
+				`RAM used : ${Logger.setColor(
+					'magenta',
+					(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
+				)} ` + Logger.setColor('magenta', 'MB')
+			);
+		}
 
-export default async (handler: CommandHandler) => {
-	function log() {
-		Logger.event(`Date : ${Logger.setColor('yellow', new Date().toString())}`);
-		Logger.event(`RAM used : ${Logger.setColor('magenta', (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))} ` + Logger.setColor('magenta', 'MB'));
-	}
+		Logger.event(
+			`Client online ! Client ${Logger.setColor(
+				'orange',
+				handler.client?.user?.username
+			)} has ${handler.client?.guilds.cache.size} guilds, it sees ${
+				handler.client?.users.cache.size
+			} users.`
+		);
 
-	Logger.event(
-		`Client online ! Client ${Logger.setColor('orange', handler.client?.user?.username)} has ${handler.client?.guilds.cache.size} guilds, it sees ${
-			handler.client?.users.cache.size
-		} users.`
-	);
-
-	log();
-	setInterval(() => {
 		log();
-	}, 20 * 60 * 1000);
-}
+		setInterval(log, 20 * 60 * 1000);
+	}
+);
