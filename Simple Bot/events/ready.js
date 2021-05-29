5d0d80a5b2394b24d9206b956503ee2e9972b511
Ayfri <pierre.ayfri@gmail.com>
2021-05-29 04:55:31 +0200

    feat: Update ready event.

--- Simple Bot/events/ready.js
+++ Simple Bot/events/ready.js
@@ -1,27 +1,40 @@
-﻿const {Logger} = require('advanced-command-handler');
-const {DateTime} = require('luxon');
+﻿const {dayjs, Event, Logger} = require('advanced-command-handler');
 
-module.exports = async handler => {
-	/**
-	 * Log information of the bot in the console.
-	 * @returns {void}
-	 */
-	function log() {
-		Logger.event(`Date : ${Logger.setColor('yellow', DateTime.local().toFormat('TT'))}`);
-		Logger.event(`RAM used  : ${Logger.setColor('magenta', (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))} ` + Logger.setColor('magenta', 'MB'));
-	}
+module.exports = new Event(
+	{
+		name: 'ready',
+		once: true,
+	},
+	async handler => {
+		/**
+		 * Log information of the bot in the console.
+		 * @returns {void}
+		 */
+		function log() {
+			Logger.event(
+				`Date : ${Logger.setColor('yellow', dayjs().format('LTS'))}`
+			);
+			Logger.event(
+				`RAM used  : ${Logger.setColor(
+					'magenta',
+					(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
+				)} ` + Logger.setColor('magenta', 'MB')
+			);
+		}
 
-	Logger.event(
-		Logger.setColor(
-			'#c0433f',
-			`Client online ! Client ${Logger.setColor('orange', handler.client.user.username, '#c0433f')} has ${handler.client.guilds.cache.size + Logger.setColor('#c0433f')} guilds, it sees ${
-				handler.client.users.cache.size + Logger.setColor('#c0433f')
-			} users.`
-		)
-	);
+		Logger.event(
+			Logger.setColor(
+				'#c0433f',
+				`Client online ! Client ${Logger.setColor(
+					'orange',
+					handler.client.user.username
+				)} has ${handler.client.guilds.cache.size} guilds, it sees ${
+					handler.client.users.cache.size
+				} users.`
+			)
+		);
 
-	log();
-	setInterval(() => {
 		log();
-	}, 20 * 60 * 1000);
-};
+		setInterval(log, 20 * 60 * 1000);
+	}
+);
